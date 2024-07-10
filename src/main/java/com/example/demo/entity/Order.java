83623eb2afddca5c84c0029a9d2982677be2225a
RBin22 <ruibin.wong0122@gmail.com>
2024-07-10 14:05:56 +0800

    new file:   src/main/java/com/example/demo/controller/OrderController.java 	new file:   src/main/java/com/example/demo/entity/Order.java 	new file:   src/main/java/com/example/demo/repository/OrderRepository.java 	new file:   src/main/java/com/example/demo/service/OrderService.java 	new file:   src/main/java/com/example/demo/service/impl/OrderServiceImpl.java 	modified:   src/main/resources/application.properties 	new file:   src/main/resources/static/css/rb_receiptstyles.css 	new file:   src/main/resources/static/css/rb_styles.css 	new file:   src/main/resources/static/images/mastercard.jpg 	new file:   src/main/resources/static/images/paypal.jpg 	new file:   src/main/resources/static/images/visa.webp 	new file:   src/main/resources/templates/checkout.html 	new file:   src/main/resources/templates/edit_order.html 	new file:   src/main/resources/templates/receipt.html

---
+++ src/main/java/com/example/demo/entity/Order.java
@@ -0,0 +1,106 @@
+ package com.example.demo.entity;
+
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.Table;
+
+@Entity
+@Table(name="orders")
+public class Order {
+
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	private Long id;
+	
+	@Column(name = "first_name", nullable=false)
+	private String firstName;
+	
+	@Column(name = "last_name", nullable=false)
+	private String lastName;
+	
+	@Column(name = "phone_number", nullable=false)
+	private String phoneNumber;
+	
+	@Column(name = "delivery_address", nullable=false)
+	private String deliveryAddress;
+	
+	@Column(name = "postcode", nullable=false)
+	private String postcode;
+	
+	@Column(name = "state", nullable=false)
+	private String state;
+
+	public Order() {}
+	
+	public Order(Long id, String firstName, String lastName, String phoneNumber, String deliveryAddress,
+			String postcode, String state) {
+		super();
+		this.id = id;
+		this.firstName = firstName;
+		this.lastName = lastName;
+		this.phoneNumber = phoneNumber;
+		this.deliveryAddress = deliveryAddress;
+		this.postcode = postcode;
+		this.state = state;
+	}
+	
+	public Long getId() {
+		return id;
+	}
+
+	public void setId(Long id) {
+		this.id = id;
+	}
+
+	public String getFirstName() {
+		return firstName;
+	}
+
+	public void setFirstName(String firstName) {
+		this.firstName = firstName;
+	}
+
+	public String getLastName() {
+		return lastName;
+	}
+
+	public void setLastName(String lastName) {
+		this.lastName = lastName;
+	}
+	
+	public String getPhoneNumber() {
+		return phoneNumber;
+	}
+
+	public void setPhoneNumber(String phoneNumber) {
+		this.phoneNumber = phoneNumber;
+	}
+
+	public String getDeliveryAddress() {
+		return deliveryAddress;
+	}
+
+	public void setDeliveryAddress(String deliveryAddress) {
+		this.deliveryAddress = deliveryAddress;
+	}
+
+	public String getPostcode() {
+		return postcode;
+	}
+
+	public void setPostcode(String postcode) {
+		this.postcode = postcode;
+	}
+
+	public String getState() {
+		return state;
+	}
+
+	public void setState(String state) {
+		this.state = state;
+	}
+}
+
